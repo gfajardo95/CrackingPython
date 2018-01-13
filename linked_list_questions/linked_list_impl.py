d55219a451ebda7a9134b1f296285ee12aab46e3
Gabriel Fajardo <gfajardo95@gmail.com>
2018-01-13 18:17:40 -0500

    implement doubly linked list

--- linked_list_questions/linked_list_impl.py
+++ linked_list_questions/linked_list_impl.py
@@ -1,95 +1,120 @@
-class ListNode:
-    """
-    A node in a singly-linked list. From Dan Bader's blog
-    """
-    def __init__(self, data=None, next=None):
-        self.data = data
-        self.next = next
+class DoubleListNode:
+  def __init__(self, data=None, next=None, prev=None):
+    self.data = data
+    self.next = next
+    self.prev = prev
+  
+  def __repr__(self):
+    return repr(self.data)
 
-    def __repr__(self):
-        return repr(self.data)
+
+class DoublyLinkedList:
+  def __init__(self, head=None):
+    self.head = head
+  
+  def append(self, data):
+    if not self.head:
+      self.head = DoubleListNode(data=data)
+      return
+    
+    curr = self.head
+    while curr.next:
+      curr = curr.next
+    curr.next = DoubleListNode(data=data, prev=curr)
+
+
+class ListNode:
+  """
+  A node in a singly-linked list. From Dan Bader's blog
+  """
+  
+  def __init__(self, data=None, next=None):
+    self.data = data
+    self.next = next
+  
+  def __repr__(self):
+    return repr(self.data)
 
 
 class SinglyLinkedList:
-
-    def __init__(self, head=None):
-        """
-        Create a new, singly-linked list. Takes O(1) time
-        """
-        self.head = head
-
-    def __repr__(self):
-        """
-        Returns a string representation of the list.
-        Takes O(n) time.
-        """
-        nodes = []
-        curr = self.head
-        while curr:
-            nodes.append(repr(curr))
-            curr = curr.next
-        return '[' + ', '.join(nodes) + ']'
-
-    def prepend(self, data):
-        """
-        Insert a new element to the beginning of the list.
-        Takes O(1) time.
-        """
-        self.head = ListNode(data=data, next = self.head)
-
-    def append(self, data):
-        """
-        Insert a new element at the end of the list.
-        Takes O(n) time.
-        """
-        if not self.head:
-            self.head = ListNode(data=data)
-            return
-        curr = self.head
-        while curr.next:
-            curr = curr.next
-        curr.next = ListNode(data=data)
-
-    def find(self, key):
-        """
-        Find a node in the list with data matching the key.
-        Takes O(n) time.
-        """
-        curr = self.head
-        while curr and curr.data != key:
-            curr = curr.next
-        return curr  # Will be None if not found
-
-    def remove(self, key):
-        """
-        Find the first occurence of key in the list and remove the pertaining node
-        Takes O(n) time.
-        """
-        curr = self.head
-        prev = None
-        while curr and curr.data != key:
-            prev = curr
-            curr = curr.next
-
-        if curr:
-            prev.next = curr.next
-            curr.next = None
-
-    def reverse(self):
-        """
-        Reverse list in place.
-        Takes O(n) time.
-        """
-        prev_node = None
-        curr = self.head
-
-        while curr:
-            next_node = curr.next
-            curr.next = prev_node
-            prev_node = curr
-            curr = next_node
-        self.head = prev_node
+  def __init__(self, head=None):
+    """
+    Create a new, singly-linked list. Takes O(1) time
+    """
+    self.head = head
+  
+  def __repr__(self):
+    """
+    Returns a string representation of the list.
+    Takes O(n) time.
+    """
+    nodes = []
+    curr = self.head
+    while curr:
+      nodes.append(repr(curr))
+      curr = curr.next
+    return '[' + ', '.join(nodes) + ']'
+  
+  def prepend(self, data):
+    """
+    Insert a new element to the beginning of the list.
+    Takes O(1) time.
+    """
+    self.head = ListNode(data=data, next=self.head)
+  
+  def append(self, data):
+    """
+    Insert a new element at the end of the list.
+    Takes O(n) time.
+    """
+    if not self.head:
+      self.head = ListNode(data=data)
+      return
+    curr = self.head
+    while curr.next:
+      curr = curr.next
+    curr.next = ListNode(data=data)
+  
+  def find(self, key):
+    """
+    Find a node in the list with data matching the key.
+    Takes O(n) time.
+    """
+    curr = self.head
+    while curr and curr.data != key:
+      curr = curr.next
+    return curr  # Will be None if not found
+  
+  def remove(self, key):
+    """
+    Find the first occurence of key in the list and remove the pertaining node
+    Takes O(n) time.
+    """
+    curr = self.head
+    prev = None
+    while curr and curr.data != key:
+      prev = curr
+      curr = curr.next
+    
+    if curr:
+      prev.next = curr.next
+      curr.next = None
+  
+  def reverse(self):
+    """
+    Reverse list in place.
+    Takes O(n) time.
+    """
+    prev_node = None
+    curr = self.head
+    
+    while curr:
+      next_node = curr.next
+      curr.next = prev_node
+      prev_node = curr
+      curr = next_node
+    self.head = prev_node
 
 
 if __name__ == '__main__':
-    pass
+  pass
